f180a3bb256e9b255edf337717a97644706ff458
amine maaoui <aminemaaoui22@gmail.com>
2020-09-30 08:48:26 +0200

    Facebook Passport

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,7 @@ var User = require('./models/user');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 var config = require('./config.js');
 
@@ -11,8 +12,8 @@ exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken = function(user) {
-    return jwt.sign(user, config.secretKey, {expiresIn: 3600});
+exports.getToken = function (user) {
+    return jwt.sign(user, config.secretKey, { expiresIn: 3600 });
 };
 
 var opts = {};
@@ -21,10 +22,10 @@ opts.secretOrKey = config.secretKey;
 
 exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
     console.log('JWT payload: ', jwt_payload);
-    User.findOne({_id: jwt_payload._id}, (err, user) => {
+    User.findOne({ _id: jwt_payload._id }, (err, user) => {
         if (err) {
             return done(err, false);
-        } 
+        }
         else if (user) {
             return done(null, user);
         }
@@ -34,16 +35,47 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => 
     });
 }));
 
-exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyUser = passport.authenticate('jwt', { session: false });
 
 exports.verifyAdmin = (req, res, next) => {
-        if(req.user.admin) {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            next();
-        } else {
-            err = new Error('You are not authorized to perform this operation');
-            res.statusCode = 403;
-            next(err);
+    if (req.user.admin) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        next();
+    } else {
+        err = new Error('You are not authorized to perform this operation');
+        res.statusCode = 403;
+        next(err);
+    }
+};
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ facebookId: profile.id }, (err, user) => {
+        if (err) {
+            return done(err, false);
         }
+        if (!err && user !== null) {
+            return done(null, user);
+        }
+        else {
+            user = new User({
+                username:
+                    profile.displayName
+            });
+            user.facebookId = profile.id;
+            user.firstname = profile.name.givenName;
+            user.lastname = profile.name.familyName;
+            user.save((err, user) => {
+                if (err)
+                    return done(err, false);
+                else
+                    return done(null, user);
+            })
+        }
+    });
 }
+
+));
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1012833699167745',
+        clientSecret: 'f5941ecc47bb1eac52b853e4009399bc'
+    }
 }
